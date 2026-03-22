a9ec40ff33f971f6baa0cc648a596165431946c9
dewKonpat <dew.konpat@gmail.com>
2026-03-22 12:26:06 +0700

    fix: build

---
+++ apps/www/src/layout/ConverterLayout/options.ts
@@ -0,0 +1,9 @@
+export const editorOptions = {
+  minimap: { enabled: false },
+  fontSize: 14,
+  lineNumbers: "on" as const,
+  scrollBeyondLastLine: false,
+  wordWrap: "on" as const,
+  automaticLayout: true,
+  tabSize: 2,
+};
